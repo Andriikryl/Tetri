c3070298130ee08c78e859b31822cb24b23040bf
Andriikryl <andreyk557@gmail.com>
2023-04-15 15:48:21 +0300

    feat[useGameOver-hook]

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,9 @@
+import { useGameOver } from "../hooks/useGameOver";
 import Menu from "./Menu";
 
 const Game = ({ rows, columns }) => {
+  const [gameOver, setGameOver, resetGameOver] = useGameOver();
+
   const start = () => {
     console.log("start");
   };
